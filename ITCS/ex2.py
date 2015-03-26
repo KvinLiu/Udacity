45549e96a818fe3253663ab9a690aad5a1cb8a2a
Kevin Liu <kevin07liu@gmail.com>
2015-03-25 22:54:07 -0700

    Change files

--- ITCS/ex2.py
+++
@@ -1,4 +0,0 @@
-print 'Hello'
-print "Hello"
-hello = "Howday"
-print hello
--- ITCS/ex2_op.py
+++
@@ -1,7 +0,0 @@
-marker = "AFK"
-replacement = "away from keyboard"
-line = "I will now go to sleep and be AFK until lunch time tomorrow."
-
-replaced = line[:line.find(marker)] + replacement + line[line.find(marker) + len(marker):]
-
-print replaced
