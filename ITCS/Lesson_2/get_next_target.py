4b71e64a426c9609e5fd3a54413883f881c4fc1f
Kevin Liu <kevin07liu@gmail.com>
2015-03-27 17:18:05 -0700

    Add a update version get_next_target

--- ITCS/Lesson_2/get_next_target.py
+++ ITCS/Lesson_2/get_next_target.py
@@ -1,10 +1,13 @@
 def get_next_target (page) :
 	start_link = page.find("<a href=")
-	start_quote = page.find('"', start_link)
-	end_quote = page.find('"', start_quote + 1)
-	url = page[start_quote + 1:end_quote]
-	return url, end_quote
+	if start_link != -1:
+		start_quote = page.find('"', start_link)
+		end_quote = page.find('"', start_quote + 1)
+		url = page[start_quote + 1:end_quote]
+		return url, end_quote
+	else:
+		return None, 0	
 
-url,endpos = get_next_target('This is a <a href="http://udacity.com">link!</a>')
+url,endpos = get_next_target('This is a')
 
-print url
+print url, endpos
