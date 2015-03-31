8fb1a51cd305d93761b58a38641071a004b7e237
Kevin Liu <kevin07liu@gmail.com>
2015-03-31 06:36:31 -0700

    Modify the daysBetweenDates function, make it better

--- ITCS/Lesson_2/daysBetweenDates.py
+++ ITCS/Lesson_2/daysBetweenDates.py
@@ -1,14 +1,12 @@
 def notsameDate(year1, month1, day1, year2, month2, day2):
-    if year1 == year2:
-        if month1 == month2:
-            if day1 == day2:
-                return False
-            else:
-                return True
-        else:
-            return True
-    else:
+    if year1 < year2:
         return True
+    if year1 == year:
+        if month1 < month2:
+            return True
+        if month1 == month2:
+            return day1 < day2
+    return False
 
 def nextDay(year, month, day):
     """Simple version: assume every month has 30 days"""
