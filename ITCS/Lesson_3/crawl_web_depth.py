c3f244260467a82ba85f9d9a7493c41163ed2af0
Kevin Liu <kevin07liu@gmail.com>
2015-05-02 23:56:47 -0700

    modify the web_crawler that can crawl depth of web

--- ITCS/Lesson_3/crawl_web_depth.py
+++ ITCS/Lesson_3/crawl_web_depth.py
@@ -9,3 +9,20 @@ def crawl_web(seed, max_depth):
             union(next_depth, get_all_links(get_page(page)))
             crawled.append(page)
         if not tocrawl:
+            tocrawl, next_depth = next_depth, []
+            depth += 1
+    return crawled
+
+def crawl_web(seed, max_depth):
+    tocrawl = [[seed]]
+    crawled = []
+    depth = 0
+    while depth < max_depth:
+        tocrawl.append([])
+        while tocrawl[depth]:
+            page = tocrawl[depth].pop()
+            if page not in crawled:
+                union(tocrawl[depth + 1], get_all_links(get_page(page)))
+                crawled.append(page)
+        depth += 1
+    return crawled
