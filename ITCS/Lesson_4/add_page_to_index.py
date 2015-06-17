28bc5fdb739161a2825a464dd53c860397ad2a0b
Kevin Liu <kevin07liu@gmail.com>
2015-06-17 11:34:14 -0700

    Add new function of webcrawer

---
+++ ITCS/Lesson_4/add_page_to_index.py
@@ -0,0 +1,13 @@
+index = []
+
+def add_to_index(index, keyword, url):
+    for entry in index:
+        if entry[0] == keyword:
+            entry[1].append(url)
+            return
+    index.append([keyword, [url]])
+
+def add_page_to_index(index, url, content):
+    keyword = content.split()
+    for k in keyword:
+        add_to_index(index, k, url)
